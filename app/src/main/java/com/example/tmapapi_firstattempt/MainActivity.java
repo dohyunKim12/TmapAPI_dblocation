7e1c6a4a5a8bc5e1dab4bd3123680f09f9ad8081
dohyunkim12 <kdh951102@gmail.com>
2021-08-03 13:47:43 +0900

    Map View success

--- app/src/main/java/com/example/tmapapi_firstattempt/MainActivity.java
+++ app/src/main/java/com/example/tmapapi_firstattempt/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.tmapapi_firstattempt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapView;
 
 public class MainActivity extends AppCompatActivity {
+    String API_KEY = "l7xx9f2b58a7392548a5985f294f0b3e125e";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayoutTmap = (LinearLayout) findViewById(R.id.linearLayoutTmap);
+        TMapView tMapView = new TMapView(this);
+
+        tMapView.setSKTMapApiKey(API_KEY);
+        linearLayoutTmap.addView(tMapView);
+
+
     }
 }
